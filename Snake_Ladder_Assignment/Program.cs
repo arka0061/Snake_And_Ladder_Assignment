0f3dafb5afae3f2a18c26cfd455a2006a384846e
Arka <arka.spectacular@gmail.com>
2021-07-22 10:57:28 +0530

    [Arka]Add:Welcome Message

---
+++ Snake_Ladder_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake_And_Ladder_Assignment
+{
+    class Program
+    {
+        public static int player_Position = 0;
+        public static  void Main(string[] args)
+        {
+            Program obj = new Program();
+           
+            Console.WriteLine("WelCome to Snake and ladder Program!");
+            Console.WriteLine("Single Player start at  position :"+ player_Position);
+         }
+       
+    }
+}
